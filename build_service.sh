c088b822b1680d3d53c25c4cb606867775bf282d
Dimitris Kagialis <kagialisd@gmail.com>
2024-04-25 18:37:41 +0300

    Add immediate start to build service script
    The build service script has been updated to immediately start the service after enabling it. This change ensures that the service not only starts on boot but is also set to run right after it has been installed and enabled.

--- build_service.sh
+++ build_service.sh
@@ -5,3 +5,6 @@ sudo cp dc-dtm.service /etc/systemd/system/
 
 # Use systemctl to enable the service. It will start on boot.
 sudo systemctl enable dc-dtm.service
+
+# Use systemctl to start the service immediately
+sudo systemctl start dc-dtm.service
