e05c23cdeb83e4b2cacd7e6074865f3d36f219d8
Dimitris Kagialis <kagialisd@gmail.com>
2024-04-25 17:21:05 +0300

    Update start_dtm.sh to include virtual environment activation
    The script now sets and activates a specified virtual environment (venv) before starting the server. It also includes a check to ensure the venv was activated successfully before proceeding, and provides user feedback regarding the success of the activation.

--- start_dtm.sh
+++ start_dtm.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-cd /home/ubuntu/experiments/dtm
+# Set the name of your venv
+VENV_NAME=venv
+
+# Navigate to project directory
+cd /home/ubuntu/DC-DTM
+
+# Activate the venv
+source $VENV_NAME/bin/activate
+
+# Check if activation was successful
+if [ $? -eq 0 ]
+then
+  echo "Virtual environment activated successfully."
+else
+  echo "Failed to activate virtual environment. Stop execution."
+  exit
+fi
+
+# Run the uvicorn server
 uvicorn main:app --reload --port 8080 --host 0.0.0.0
